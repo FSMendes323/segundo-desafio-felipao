8206902954182028427533463ccffbd4ece97ee9
FSMendes323 <fsm152@hotmail.com>
2024-11-01 15:39:09 -0300

    versão final

--- index.js
+++ index.js
@@ -29,15 +29,15 @@ const heroi = [{
 }];
 
 function subtrair(vitorias, derrotas) {
-  return vitorias - derrotas;
+     return vitorias - derrotas;
   }
   
 for (let i = 0; i < heroi.length; i++) {
-let nome = heroi[i].nome;  
-let vitorias = heroi[i].vitorias;
-let derrotas = heroi[i].derrotas;
-let saldo = subtrair(vitorias,derrotas);
-let nivel = "";
+    let nome = heroi[i].nome;  
+    let vitorias = heroi[i].vitorias;
+    let derrotas = heroi[i].derrotas;
+    let saldo = subtrair(vitorias,derrotas);
+    let nivel = "";
 
 console.log("O Herói " + nome + " tem saldo de " + saldo + " está no nível " + nivel) 
 
@@ -59,32 +59,3 @@ if (saldo <= 10){
   }
 }
 
-/*
-Instruções para entrega
- # 2️⃣ Calculadora de partidas Rankeadas
-**O Que deve ser utilizado**
-
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-- Funções
-
-## Objetivo:
-
-Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
-
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
- */
